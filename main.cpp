6ef742f25760281afa4ee850127661ab64e2cbb1
Zeotical <BASMALA.MOHAMED.ALI@student.mmu.edu.my>
2024-12-08 18:49:24 +0800

    info abt group

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+// ********************************************************* 
+// Program: YOUR_FILENAME.cpp 
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: 
+// Tutorial Class: 
+// Trimester: 2430
+// Member_1: ID | NAME | EMAIL | PHONE
+// Member_2: ID | NAME | EMAIL | PHONE
+// Member_3: ID | NAME | EMAIL | PHONE
+// Member_4: ID | NAME | EMAIL | PHONE
+// *********************************************************
+// Task Distribution
+// Member_1: 
+// Member_2: 
+// Member_3: 
+// Member_4:
+// *********************************************************
+
 #include <iostream>
 
 using namespace std;
